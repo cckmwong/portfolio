fd3c87d79d1ab01544eb2222361c0784d711c585
Carmen Wong <106394888+cckmwong@users.noreply.github.com>
2023-08-15 12:23:29 +0800

    Update and rename data cleaning_normalization.py to data cleaning.py

---
+++ codes/UK living/data cleaning.py
@@ -0,0 +1,9 @@
+#Find the required data for each local authority, e.g. housing prices, Key Stage 2 results, etc. 
+dataset.drop(dataset[dataset['KS2_Avg'] == 0].index, inplace=True)
+grouped = dataset.groupby(['LA code'],as_index=False).mean() 
+
+#Normalization
+dataset['KS2_norm'] = (dataset['KS2_Avg'] - dataset['KS2_Avg'].min())/ (dataset['KS2_Avg'].max()-dataset['KS2_Avg'].min())
+
+#For normalizing Income Deprivation and Crime, the normalized value is substracted by 1, as a lower value represents a higher affluency or better safety respectively
+dataset['ID_norm'] = 1- ((dataset['AvgID'] - dataset['AvgID'].min())/ (dataset['AvgID'].max()-dataset['AvgID'].min()))
--- codes/UK living/data cleaning_normalization.py
+++
@@ -1,6 +0,0 @@
-#Use of Python codes for normalization
-dataset['KS2_norm'] = (dataset['KS2_Avg'] - dataset['KS2_Avg'].min())/ (dataset['KS2_Avg'].max()-dataset['KS2_Avg'].min())
-
-#Normalization of Income Deprivation and Crime
-#Normalized value is substracted by 1, as a lower value represents a higher affluency or better safety respectively
-dataset['ID_norm'] = 1- ((dataset['AvgID'] - dataset['AvgID'].min())/ (dataset['AvgID'].max()-dataset['AvgID'].min()))
