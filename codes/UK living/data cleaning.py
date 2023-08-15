ba019ee80777214532a93ecf048cf2b4e53ccedd
Carmen Wong <106394888+cckmwong@users.noreply.github.com>
2023-08-15 12:25:27 +0800

    Update data cleaning.py

--- codes/UK living/data cleaning.py
+++ codes/UK living/data cleaning.py
@@ -2,7 +2,7 @@
 dataset.drop(dataset[dataset['KS2_Avg'] == 0].index, inplace=True)
 grouped = dataset.groupby(['LA code'],as_index=False).mean() 
 
-#Normalization
+#Normalization of different indicators
 dataset['KS2_norm'] = (dataset['KS2_Avg'] - dataset['KS2_Avg'].min())/ (dataset['KS2_Avg'].max()-dataset['KS2_Avg'].min())
 
 #For normalizing Income Deprivation and Crime, the normalized value is substracted by 1, as a lower value represents a higher affluency or better safety respectively
